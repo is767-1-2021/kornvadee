34604b756c65b924bbb91f697541f188425d5e88
Kornvadee Ounjit <Kornvadee.oun@gmail.com>
2021-08-25 21:21:06 +0700

    Add four pages

--- first_app/lib/main.dart
+++ first_app/lib/main.dart
@@ -6,41 +6,32 @@ void main() {
 // ^ start application
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     // function build คือเรียกค่าบนมือถือ
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-      ),
-      home: MyHomePage(title: 'Pop Cat App'),
+          primarySwatch: Colors.blue,
+          primaryColor: Colors.amber[200],
+          accentColor: Colors.red[300],
+          textTheme: TextTheme(bodyText2: TextStyle(color: Colors.grey[900]))),
+      initialRoute: '/forth',
+      // เป็นตัวกำหนดได้ว่าหน้าแรกของแอปจะไปหน้าไหน
+      routes: <String, WidgetBuilder>{
+        '/first': (context) => FirstPage(),
+        '/second': (context) => SecondPage(),
+        '/third': (context) => ThirdPage(),
+        '/forth': (context) => FourthPage(),
+        // ใส่หน้าใหม่อย่าลืม มา set route ด้านบนตรงนี้ด้วย โดยเชื่อมมาด้วย class
+        // เวลา reload หน้าที่เพิ่มเข้ามาใหม่ใช้ R (not r)
+      },
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
   MyHomePage({Key? key, required this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
   final String title;
 
   @override
@@ -70,11 +61,6 @@ class _MyHomePageState extends State<MyHomePage> {
     setState(() {
       cat = cat2;
       //เป็นการสั่งว่าถ้าจะ refresh แล้วให้เก็บค่า >> ต้องใช้ setstate เสมอ
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
       _counter += 1;
     });
   }
@@ -83,51 +69,37 @@ class _MyHomePageState extends State<MyHomePage> {
     setState(() {
       cat = cat1;
       //เป็นการสั่งว่าถ้าจะ refresh แล้วให้เก็บค่า >> ต้องใช้ setstate เสมอ
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
       _counter -= 1;
     });
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
         title: Text(widget.title),
       ),
       body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
         child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
           mainAxisAlignment: MainAxisAlignment.center,
           // ให้ข้อมูลอยู่ตรง center/ start/end...
           children: <Widget>[
-            cat,
+            Container(
+              // เอา containter มาช่วยล้อคขนาดกรอบได้
+              height: 150.0,
+              // ความสูงของกรอบ
+              margin: EdgeInsets.only(left: 100.0, right: 100.0, bottom: 100.0),
+              // ล้อกตำแหน่งกรอบ (ขนาดจาก... เช่น ห่างจากฝั่งซ้าย 100 )
+              padding: EdgeInsets.all(8.0),
+              decoration: BoxDecoration(
+                color: Colors.amber.withOpacity(0.25),
+                // สีในขอบ opacity คือความทึบ
+                borderRadius: BorderRadius.circular(10.0),
+                // ความมนของขอบ
+              ),
+              child: cat,
+            ),
+
             Text(
               'You have pushed the button this many times:',
             ),
@@ -159,7 +131,7 @@ class _MyHomePageState extends State<MyHomePage> {
         onPressed: _incrementCounter,
         tooltip: 'Increment',
         child: Icon(Icons.inventory),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+      ),
     );
   }
 }
@@ -169,12 +141,173 @@ class SubmitButton extends StatelessWidget {
   SubmitButton(this.buttonText);
 // ต้องรับค่าตัวหนังสือที่มาจากที่กำหนดมาด้วย (link เข้ามาให้)
   @override
+  // @ override เป็นการบอกว่า contribute นี้เราจะเขียนเอง ให้เอา format มา
   Widget build(context) {
     return ElevatedButton(
       child: Text(this.buttonText),
       onPressed: () {
         print('Pressed');
       },
+    );
+  }
+}
+
+class FirstPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('First Page'),
+        actions: [
+          IconButton(onPressed: () {}, icon: Icon(Icons.arrow_forward)),
+          IconButton(onPressed: () {}, icon: Icon(Icons.agriculture)),
+          IconButton(onPressed: () {}, icon: Icon(Icons.bubble_chart)),
+          IconButton(onPressed: () {}, icon: Icon(Icons.camera_roll)),
+          IconButton(onPressed: () {}, icon: Icon(Icons.travel_explore)),
+        ],
+      ),
+    );
+  }
+}
+
+class SecondPage extends StatelessWidget {
+  @override
+  // กด Ctrl + . เพื่อดูวิธี error correction
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Second Page'),
+        centerTitle: true,
+      ),
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.mail_outline),
+        onPressed: () {},
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text('Here is the text formatted by theme data'),
+            Table(
+              children: [
+                TableRow(children: [
+                  Container(
+                    child: Text('Number'),
+                    decoration: BoxDecoration(
+                        color: Colors.green[100],
+                        borderRadius: BorderRadius.circular(8.0)),
+                  ),
+                  Container(
+                    child: Text('Name'),
+                    decoration: BoxDecoration(
+                        color: Colors.green[100],
+                        borderRadius: BorderRadius.circular(8.0)),
+                  ),
+                  Container(
+                    child: Text('Gender'),
+                    decoration: BoxDecoration(
+                        color: Colors.green[100],
+                        borderRadius: BorderRadius.circular(8.0)),
+                  ),
+                ]),
+                TableRow(children: [
+                  Text('1'),
+                  Text('Inging'),
+                  Text('Female'),
+                ]),
+                TableRow(children: [
+                  Text('2'),
+                  Text('Name 2'),
+                  Text('Male'),
+                ]),
+              ],
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
+// สร้างเสร็จแล้วอย่าลืมดึงขั้นไปใส่ตรง route ด้านบนด้วย
+
+class ThirdPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return DefaultTabController(
+      initialIndex: 0,
+      // ตัวเริ่มต้นของ tab นับเปน 0,1,2 เช่นถ้า 0 = cloud
+      length: 3,
+      child: Scaffold(
+        appBar: AppBar(
+          title: Text('Third Page'),
+          bottom: TabBar(
+            tabs: [
+              Tab(icon: Icon(Icons.cloud)),
+              Tab(icon: Icon(Icons.beach_access_outlined)),
+              Tab(icon: Icon(Icons.brightness_1_outlined)),
+            ],
+          ),
+        ),
+        body: TabBarView(
+          children: [
+            Center(
+              child: Text('Cloud'),
+            ),
+            Center(
+              child: Text('Umbrella'),
+            ),
+            Center(
+              child: Text('Sunny'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class FourthPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    final List<String> entries = <String>[
+      'A',
+      'B',
+      'C',
+      'D',
+      'E',
+      'F',
+      'G',
+      'H',
+      'I',
+      'J',
+      'K',
+      'L',
+      'M',
+      'N',
+      'O'
+    ];
+    final List<int> colorCodes = <int>[700, 400, 100];
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Listview Example'),
+        // Listview ทำให้สามารถเลื่อนหน้าจอนึง กล่องก้เพิ่มเข้ามา (สร้างทีละตัวเมื่อกล่องโผล่เข้ามาในจอ)
+      ),
+      body: ListView.separated(
+        padding: EdgeInsets.all(9.0),
+        itemCount: entries.length,
+        itemBuilder: (context, index) {
+          return Container(
+            height: 50,
+            color: Colors.amberAccent[colorCodes[index % 3]],
+            // ดึงจาก list colorcodes
+            child: Center(
+              child: Text('Entry ${entries[index]}'),
+              // ดึงข้อความ Entry และ [list entries]
+            ),
+          );
+        },
+        separatorBuilder: (context, index) => Divider(),
+      ),
     );
   }
 }
